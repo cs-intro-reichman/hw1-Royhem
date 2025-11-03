f6248eec8db131de6eb7643c86734d7e90f069aa
Royhem <roy.hemelniker@post.runi.ac.il>
2025-11-03 09:33:14 +0200

    Update Bill3.java

--- Bill3.java
+++ Bill3.java
@@ -9,7 +9,7 @@ public class Bill3 {
 		String name3= args[2];
         double total = Double.parseDouble(args[3]);
 		double count = Math.ceil(total/3);
-		System.out.println("Dear " + name3 + ", "+ name2 + " and "+ name1 + ": Pay " +  (count)+ " Shekels each." );
+		System.out.println("Dear " + name3 + ", "+ name2 + ", and "+ name1 + ": Pay " +  (count)+ " Shekels each." );
 		   
 
 
